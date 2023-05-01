368d85ed8db5394128bca49deefbed52581942a7
ariful-islam <arifulislam204909@gmail.com>
2023-05-02 01:07:59 +0600

    website name added

---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,32 @@
+/*eslint-disable */
+import Button from "react-bootstrap/Button";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+            <Nav>
+            <Navbar.Brand href="#home">
+            Cooking Experts
+          </Navbar.Brand>
+            </Nav>
+          <Nav className="mx-auto">
+          
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Blogs</Nav.Link>
+            
+          </Nav>
+          <Button variant="danger">Login</Button>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+/*eslint-disable */
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
+import Main from "../layout/main";
+
+const router = createBrowserRouter(
+    [
+        {
+            path:'/',
+            element:<Main></Main>,
+            children:[
+                {
+                    path:'/',
+                    element:<Home></Home>
+                }
+            ]
+        }
+    ]
+);
+export default router;
