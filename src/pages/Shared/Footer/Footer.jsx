9a2dc381d2f895ca860a34c386b0aaf56d7190bb
ariful-islam <arifulislam204909@gmail.com>
2023-05-03 01:10:52 +0600

    footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,8 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1 className="text-red-500">this is footer</h1>
+    <div className="bg-slate-800 text-white w-full h-52 ">
+      <div className="flex gap-64 justify-center">
+        <div className="mt-10">
+          <h3 className="font-bold">Explore:</h3>
+          <p>Home</p>
+          <p>About</p>
+          <p>Blog</p>
+        </div>
+        <div className="mt-10">
+          <h3 className="font-bold">Visit Us:</h3>
+          <p>CA United States</p>
+        </div>
+        <div className="mt-10">
+          <h3 className="font-bold">Follow Us:</h3>
+          <p>Instagram</p>
+          <p>Facebook</p>
+          <p>Linkedin</p>
+        </div>
+        <div className="mt-10">
+          <h3 className="font-bold">Legal:</h3>
+          <p>Terms</p>
+          <p>Privacy</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Shared/Navbar/NavBar.jsx
+++ src/pages/Shared/Navbar/NavBar.jsx
@@ -4,15 +4,15 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <div className='bg-slate-800'><div className='flex justify-between ml-56 me-56 mt-5 items-center'>
+            <div className='bg-slate-800 w-full h-20 align-items-center'><div className='flex justify-between ml-56 me-56 mt-5 items-center'>
             <h2 className='font-bold text-white	 text-lg '>Cooking Experts</h2>
             <div className='flex gap-5 text-white'>
-                <NavLink to='/' className={({ isActive }) => (isActive ? 'text-red-600' : '')}>Home</NavLink>
-                <NavLink to='/blog' className={({ isActive }) => (isActive ? 'text-red-600' : '')}>Blog</NavLink>
+                <NavLink to='/' className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}>Home</NavLink>
+                <NavLink to='/blog' className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}>Blog</NavLink>
 
             </div>
             <button className="bg-indigo-500 rounded-lg p-3 text-white">
-                Star Applying
+                Login
             </button>
         </div></div>
         </div>
