f2aa36f0e83aad17d411982bd59839c0e9c80ba5
ariful-islam <arifulislam204909@gmail.com>
2023-05-05 23:35:32 +0600

    google and github btn added on the register page

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
+import LoginWithSocial from "./LoginWithSocial/LoginWithSocial";
 
 const Register = () => {
   const { createUser, userProfile, userUpdate } = useContext(AuthContext);
@@ -48,76 +49,75 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <div className="text-center">
-        <h3 className="font-bold text-3xl mt-6">Please Register</h3>
-        <form action="" className="mt-12" onSubmit={handleRegister}>
-          <p className="text-red-600">{error}</p>
-          <div className="">
-            <label htmlFor="" className="font-bold">
-              Name
-            </label>
-            <br />
-            <input
-              type="text"
-              placeholder="Your Name"
-              className="input input-bordered input-primary w-full max-w-xs mt-3"
-              name="name"
-            />
-          </div>
+    <div className="text-center mb-10">
+      <h3 className="font-bold text-3xl mt-6">Please Register</h3>
+      <form action="" className="mt-12" onSubmit={handleRegister}>
+        <p className="text-red-600">{error}</p>
+        <div className="">
+          <label htmlFor="" className="font-bold">
+            Name
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="input input-bordered input-primary w-full max-w-xs mt-3"
+            name="name"
+          />
+        </div>
 
-          <div className="">
-            <label htmlFor="" className="font-bold">
-              Email
-            </label>
-            <br />
+        <div className="">
+          <label htmlFor="" className="font-bold">
+            Email
+          </label>
+          <br />
 
-            <input
-              type="email"
-              placeholder="Your email"
-              className="input input-bordered input-primary w-full max-w-xs mt-3"
-              name="email"
-            />
-          </div>
-          <div className="mt-6">
-            <label htmlFor="" className="font-bold">
-              Password
-            </label>
+          <input
+            type="email"
+            placeholder="Your email"
+            className="input input-bordered input-primary w-full max-w-xs mt-3"
+            name="email"
+          />
+        </div>
+        <div className="mt-6">
+          <label htmlFor="" className="font-bold">
+            Password
+          </label>
 
-            <br />
-            <input
-              type="password"
-              placeholder="Your password"
-              className="input input-bordered input-primary w-full max-w-xs mt-3"
-              name="password"
-            />
-          </div>
-          <div className="mt-6">
-            <label htmlFor="" className="font-bold">
-              Photo URL
-            </label>
-            <br />
-            <input
-              type="photo"
-              placeholder="Your Photo URL"
-              className="input input-bordered input-primary w-full max-w-xs mt-3"
-              name="photo"
-            />
-          </div>
-          <button
-            className="btn btn-active btn-primary w-full max-w-xs mt-6"
-            type="submit"
-          >
-            Submit
-          </button>
-          <p className="mt-6 mb-6">
-            Already have an Account?
-            <Link to="/login" className="font-bold  text-blue-600">
-              Login
-            </Link>
-          </p>
-        </form>
-      </div>
+          <br />
+          <input
+            type="password"
+            placeholder="Your password"
+            className="input input-bordered input-primary w-full max-w-xs mt-3"
+            name="password"
+          />
+        </div>
+        <div className="mt-6">
+          <label htmlFor="" className="font-bold">
+            Photo URL
+          </label>
+          <br />
+          <input
+            type="photo"
+            placeholder="Your Photo URL"
+            className="input input-bordered input-primary w-full max-w-xs mt-3"
+            name="photo"
+          />
+        </div>
+        <button
+          className="btn btn-active btn-primary w-full max-w-xs mt-6"
+          type="submit"
+        >
+          Submit
+        </button>
+        <p className="mt-6 mb-6">
+          Already have an Account?
+          <Link to="/login" className="font-bold  text-blue-600">
+            Login
+          </Link>
+        </p>
+      </form>
+      <LoginWithSocial></LoginWithSocial>
     </div>
   );
 };
